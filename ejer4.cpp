a0961dfc3be5fd6267e69003d58b7f556a0934db
Fran Acién <amil101ftw@gmail.com>
2017-03-11 13:59:08 +0100

    Añandiendo ejer4

--- ejer4.cpp
+++ ejer4.cpp
@@ -1,46 +1,52 @@
 /*
  * ejer4.cpp
  *
- *  Created on: 23/2/2017
- *      Author: bn0336
+ *  Created on: 4 mar. 2017
+ *      Author: amil101
+ *
+ *	Ejercicio de Pilas, que recibiendo una pila y un numero entero x, si la clave
+ *	x se encuentra en la pila. devuelva la suma de las claves de la misma hasta llegar a x
+ *	(sin incluir la clave x). Si la clave x no se encuentra en la pila, el método devuelve
+ *	la suma de todas sus claves
+ *
  */
 #include <iostream>
+#include <stack>
 using namespace std;
 
-#include "Pila.h"
-#include "Cola.h"
+int sumaHasta(stack<int>  &pila, int x){
+	int elem, suma = 0;
 
-
-void meterEnColaYPilaRecur(Cola &cola, Pila &pila, int elemCola){
-	int x;
-	if(elemCola > 0){
-		x = cola.desencolar();
-		if(!(elemCola % 2)){		//Par
-			pila.apilar(x);
+	if (!pila.empty()){
+		elem = pila.top();
+		pila.pop();
+		if(x == elem){			//Si la encuentra se da la vuelta
+			pila.push(elem);
+			return 0;
 		}
-		else{
-			cola.encolar(x);
-		}
-		meterEnColaYPilaRecur(cola, pila, elemCola - 1);
-
+		suma += elem;
+		suma += sumaHasta(pila, x);
+		pila.push(elem);
 	}
+	return suma;
 }
 
 int main(){
-	Cola a;
-	Pila b;
+	stack<int> pila1;
 
-	a.encolar(1);
-	a.encolar(3);
-	a.encolar(2);
-	a.encolar(4);
-	a.encolar(5);
-	a.encolar(1);
-	a.imprimirCola();
+	pila1.push(-7);
+	pila1.push(0);
+	pila1.push(-9);
+	pila1.push(2);
+	pila1.push(4);
+	pila1.push(4);
 
-	meterEnColaYPilaRecur(a, b, a.numElemCola());
-	a.imprimirCola();
-	b.imprimirPila();
+	cout << "Sumar hasta 5: RESULTADO=" << sumaHasta(pila1, -9) << endl;
+
+ 	pila1.~stack();
 	return 0;
 }
+
+
+
 
